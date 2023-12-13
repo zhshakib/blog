2a3c5cfc53e92cc393b701d82b4b7043801d699a
zhshakib <zhshakib@gmail.com>
2023-12-14 05:43:56 +0600

    chore: Navbar customization

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
-  <header w-full flex flex-items-center>
-    <div border-b="1px solid bottom primary" m="x-auto y-0" mb-2 class="flex flex-justify-between flex-items-center" w-full py-2 container>
-      <a pt-5 text-6xl font-title>NoobZH!</a>
+  <header w-full flex flex-items-center drop-shadow>
+    <div m="x-auto y-0" class="flex flex-justify-around flex-items-center" mb-2 w-full py-2 drop-shadow container>
+      <a pt-10 text-6xl font-title>NoobZH!</a>
       <div font-roboto class="text-[18px] uppercase">
-        <a class="text-bold text-primary" border="1px solid primary" mx-5 btn hover="text-secondary border-b border-secondary" p="x-10 y-2">Blog</a>
-        <a class="text-bold text-primary" border="1px solid primary" mx-5 btn hover="text-secondary border-b border-secondary" p="x-10 y-2">About</a>
+        <a class="text-bold text-dark dark:text-white" border-b="1px solid primary" mx-5 btn hover="text-secondary border-b border-secondary" p="x-10 y-2">Blog</a>
+        <a class="text-bold text-dark dark:text-white" border-b="1px solid primary" mx-5 btn hover="text-secondary border-b border-secondary" p="x-10 y-2">About</a>
       </div>
     </div>
   </header>
