f7a7d44db6e4f989ac8cafcc1e3e8cf979b972d5
zhshakib <zhshakib@gmail.com>
2023-12-14 04:18:37 +0600

    feat: navbar Done!

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
   <header w-full flex flex-items-center>
-    <div border-b="1px solid bottom" m="x-auto y-0" class="flex flex-justify-between flex-items-center" w-full py-2 container>
+    <div border-b="1px solid bottom primary" m="x-auto y-0" mb-2 class="flex flex-justify-between flex-items-center" w-full py-2 container>
       <a pt-5 text-6xl font-title>NoobZH!</a>
-      <div font-poppins text-1xl>
-        <a class="text-very-cool" mx-5 btn hover="border-b-2px border-rounded" p="x-10 y-2">Blogs</a>
-        <a class="text-very-cool" mx-5 btn hover="border-b-2px border-rounded" p="x-10 y-2">About</a>
+      <div font-roboto class="text-[18px] uppercase">
+        <a class="text-bold text-primary" border="1px solid primary" mx-5 btn hover="text-secondary border-b border-secondary" p="x-10 y-2">Blog</a>
+        <a class="text-bold text-primary" border="1px solid primary" mx-5 btn hover="text-secondary border-b border-secondary" p="x-10 y-2">About</a>
       </div>
     </div>
   </header>
--- uno.config.ts
+++ uno.config.ts
@@ -27,22 +27,12 @@ export default defineConfig({
 
   theme: {
     colors: {
-      primary: '#0070f3',
-      gray: {
-        100: '#f7fafc',
-        200: '#edf2f7',
-        300: '#e2e8f0',
-        400: '#cbd5e0',
-        500: '#a0aec0',
-        600: '#718096',
-        700: '#4a5568',
-        800: '#2d3748',
-        900: '#1a202c',
-      },
+      primary: '#42b883',
+      secondary: '#F0DB4F',
     },
   },
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-primary text-white cursor-pointer hover:bg-primary disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['btn', 'px-4 py-1 rounded inline-block bg-transparent text-white cursor-pointer disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600 !outline-none'],
   ],
 })
