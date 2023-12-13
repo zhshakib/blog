e7ae4d97da8de9c305d80fea4f8d464e40e05c82
zhshakib <zhshakib@gmail.com>
2023-12-14 04:24:49 +0600

    chore: working on Hero!

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,8 +5,11 @@
     <div mx-auto flex flex-row items-center justify-between container>
       <div>
         <h2 font="mono italic" my-5 text-6xl>
-          Ziaul Hasan Shakib
+          Ziaul Hasan <span color="primary" font="mono light" class="uppercase underline">Shakib</span>
         </h2>
+        <p font="roboto medium " text-2xl>
+          Computer Science<span color="#42b883" font="mono bold"> Student</span> <span i-carbon-favorite />
+        </p>
         <p font="roboto medium " text-2xl>
           Backend Developer, <span color="#42b883" font="mono bold"> Vue.js</span> <span i-carbon-favorite />
         </p>
